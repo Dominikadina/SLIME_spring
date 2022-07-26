36ee6c655f6e42fdf600afc27ac311ce061b1872
Dominika <dominika.d.gacek@gmail.com>
2022-07-26 14:09:24 +0200

    start commit

---
+++ src/main/java/pl/sda/arppl4/slim_spring/SlimSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.arppl4.slim_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlimSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlimSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/arppl4/slim_spring/SlimSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.arppl4.slim_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlimSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
